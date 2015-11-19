f323988e1404f58c301084b4ae05191efbc00c3f
kondrashov <kondrashov@KONDRASHOV.main.dom>
2015-11-19 17:57:08 +0400

    fix

--- src/java/com/evgen/common/controller/HelloController.java
+++ src/java/com/evgen/common/controller/HelloController.java
@@ -13,15 +13,35 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
-@RequestMapping("/welcome")
+@RequestMapping(method = RequestMethod.GET)
+//@RequestMapping("/welcome")
 public class HelloController {
-
-	@RequestMapping(method = RequestMethod.GET)
+        @RequestMapping("/welcome")
+	// @RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 
 		model.addAttribute("message", "Велком!");
 		return "hello";
 
 	}
-	
+        
+        
+        @RequestMapping("/goodbye")
+	// @RequestMapping(method = RequestMethod.GET)
+	public String printGoodbye(ModelMap model) {
+
+		model.addAttribute("message", "Вали отсель!");
+		return "hello";
+
+	}
+        
+        @RequestMapping("/")
+	// @RequestMapping(method = RequestMethod.GET)
+	public String printNothing(ModelMap model) {
+
+		model.addAttribute("message", "Ничё нет!");
+		return "nothing";
+
+	}
+        
 }
